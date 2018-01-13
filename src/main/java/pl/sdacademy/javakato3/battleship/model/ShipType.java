9405249d1efa40bc1e0bff9eda079456db316808
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-01-13 13:17:25 +0100

    Added model classes.

---
+++ src/main/java/pl/sdacademy/javakato3/battleship/model/ShipType.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.javakato3.battleship.model;
+
+public enum ShipType {
+    BATTLESHIP(4),
+    CRUISER(3),
+    DESTROYER(2),
+    SUBMARINE(1);
+
+    private final Integer size;
+
+    ShipType(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+}
