e7b9bc9b1a5507da7962d9ee2246f5fe76bc26e4
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-01-14 13:03:41 +0100

    Validation of ship placement implemented

---
+++ src/main/java/pl/sdacademy/javakato3/battleship/validation/StartOnAMapValidator.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.javakato3.battleship.validation;
+
+import pl.sdacademy.javakato3.battleship.model.PlayersBoard;
+import pl.sdacademy.javakato3.battleship.model.Ship;
+
+public class StartOnAMapValidator implements Validator {
+    @Override
+    public boolean isValid(Ship ship, PlayersBoard board) {
+        int shipX = (int) ship.getPosition().getX();
+        int shipY = (int) ship.getPosition().getY();
+
+        return shipX >=0 && shipX < 10 && shipY >=0 && shipY < 10;
+    }
+}
