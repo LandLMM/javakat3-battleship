87c089dea057811fab6b99d141a97a55050bfb74
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-01-20 19:29:13 +0100

    Game board is observable

--- src/main/java/pl/sdacademy/javakato3/battleship/model/GameBoard.java
+++ src/main/java/pl/sdacademy/javakato3/battleship/model/GameBoard.java
@@ -1,12 +1,33 @@
 package pl.sdacademy.javakato3.battleship.model;
 
+import pl.sdacademy.javakato3.battleship.ui.UserInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameBoard {
     private PlayersBoard humanPlayerBoard;
     private PlayersBoard otherPlayerBoard;
+    private List<UserInterface> observers;
 
     public GameBoard(PlayersBoard humanPlayerBoard, PlayersBoard otherPlayerBoard) {
         this.humanPlayerBoard = humanPlayerBoard;
         this.otherPlayerBoard = otherPlayerBoard;
+        observers = new ArrayList<>();
+    }
+
+    public void registerUI(UserInterface ui) {
+        observers.add(ui);
+    }
+
+    public void unregisterUI(UserInterface ui) {
+        observers.remove(ui);
+    }
+
+    public void updateObservers() {
+        for (UserInterface observer : observers) {
+            observer.printMaps(this);
+        }
     }
 
     public PlayersBoard getHumanPlayerBoard() {
